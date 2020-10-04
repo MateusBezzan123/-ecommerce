37f7d3a74fff710ca9eee06a950ec8c647c24813
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-10-04 16:31:22 -0300

    ADD Form do app

--- AppTab.js
+++ AppTab.js
@@ -1,41 +1,3 @@
-5;
-6;
-7;
-8;
-9;
-10;
-11;
-12;
-13;
-14;
-15;
-16;
-17;
-18;
-19;
-20;
-21;
-22;
-23;
-24;
-25;
-26;
-27;
-28;
-29;
-30;
-31;
-32;
-33;
-34;
-35;
-36;
-37;
-38;
-39;
-40;
-41;
-42;
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -72,6 +34,20 @@ function AppTab() {
       >
         <Screen name="AppList" component={AppList} />
         <Screen name="AppForm" component={AppForm} />
+        <Screen
+          name="AppList"
+          component={AppList}
+          options={{
+            tabBarLabel: "Compras",
+          }}
+        />
+        <Screen
+          name="AppForm"
+          component={AppForm}
+          options={{
+            tabBarLabel: "Adicionar",
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
